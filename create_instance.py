1b472dcf211e182433f4bf9340471025aeb61b90
Yogesh Upadhyay <yogesu@amazon.com>
2018-11-10 21:56:36 +0000

    Basics: EC2 resource operations

---
+++ create_instance.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import boto3
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+        ImageId='ami-047bb4163c506cd98',
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro')
+print instance[0].id
